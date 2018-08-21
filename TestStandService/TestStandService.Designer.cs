43224cbb6955c70e6e26dc83eb042206dbaf79c3
roxanakarami <roxana.karami@ni.com>
2018-08-21 15:52:57 -0500

    Adding repo for the first time
    intial push

---
+++ TestStandService/TestStandService.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace TestStandService
+{
+    partial class TestStandService
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private int eventId = 1;
+
+
+
+
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            // 
+            // TestStandService
+            // 
+            this.ServiceName = "Service1";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+
+        }
+
+        #endregion
+        //adding a decleration for the event log
+        private System.Diagnostics.EventLog eventLog1;
+    }
+}
